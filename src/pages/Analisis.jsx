a953ce751713b0837c40ff189180e94595c96921
jose <joseluis836@hotmail.com>
2022-10-18 06:44:28 -0500

    Se creo el formulario de seleccion de fechas

--- src/pages/Analisis.jsx
+++ src/pages/Analisis.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Analisis.css';
 
 const Analisis = () => {
   return (
-    <div>Analisis</div>
+    <section className='Analisis'>
+      <h2>Análisis Alarmas</h2> 
+      <form action="" >
+        <fieldset>
+        <legend>Elegir el intervalo de tiempo</legend>
+          <fieldset className='campo'>
+            <label htmlFor="">Inicio</label>
+            <input type="date" />
+          </fieldset>
+          <fieldset className='campo'>
+            <label htmlFor="">Fin</label>
+            <input type="date" />
+          </fieldset>
+          <button className='btn btn-enviar'>Enviar</button>
+        </fieldset>
+      </form>
+    </section>
   )
 }
 
